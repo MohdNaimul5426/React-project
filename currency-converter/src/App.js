3ccbbe04458bbc15fc735700ec1ca85a9278bc92
Mohd Naimul <mohdnaimul111@gmail.com>
2024-07-09 16:04:40 +0530

    fifth commit

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,11 +1,45 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import Axios from 'axios'
 import Dropdown from 'react-dropdown'
+import { HiSwitchHorizontal } from "react-icons/hi";
+import 'react-dropdown/style.css'
 
 
 import './App.css';
 
 function App() {
+  const [info,setInfo]=useState([])
+  const [input,setInput]=useState(0)
+  const [from,setFrom]=useState("usd")
+  const [to,setTo]=useState("inr")
+  const [options,setOptions]=useState([])
+  const [output,setOutput]=useState(0)
+//calling the api whenever the dependency changes
+  useEffect(() =>{
+    Axios.get(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`)
+    .then((res) =>{
+      setInfo(res.data[from]);
+    })
+  },[from]);
+//calling the convert function whenever a user switches the currency
+useEffect(() =>{
+  setOptions(Object.keys(info));
+  console.log(info);
+  convert();
+},[info]);
+
+const convert=() =>{
+  let rate =info[to]
+  setOutput(input*rate);
+}
+const flip=() =>{
+  let temp=from;
+  setFrom(to)
+  setTo(temp)
+}
+
+
   return (
     <div className="App">
      <div className="heading">
@@ -14,25 +48,34 @@ function App() {
      <div className="container">
       <div className="left">
         <h3>Amount</h3>
-        <input type="text" placeholder='Enter the amount' />
+        <input type="text" placeholder='Enter the amount'
+        onChange={(e) => setInput(e.target.value)} />
       </div>
       <div className="middle">
         <h3>From</h3>
-        <Dropdown />
+        <Dropdown options={options} onChange={(e) => setFrom(e.value)}
+          value={from} placeholder="From"/>
       </div>
-      <div className="switch"></div>
+      <div className="switch">
+        <HiSwitchHorizontal size="30px"
+        onClick={()=>{flip()}}/>
+      </div>
       <div className="right">
         <h3>To</h3>
-        <Dropdown/>
+        <Dropdown options={options}
+        onChange={(e) => setTo(e.value)}
+        value={to} placeholder="To"/>
       </div>
      </div>
      <div className="result">
-      <button>Convert</button>
+      <button onClick={() => convert()}>Convert</button>
       <h2>Converted Amount:</h2>
-      <p></p>
+      <p>{input + " " + from+ " = "+ output.toFixed(2) + " " + to}</p>
      </div>
     </div>
   );
 }
+
+
 
 export default App;
